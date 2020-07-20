596fdd1f48adb4f6c7773658d3962d722384759d
Oran <42975732+ItsOran@users.noreply.github.com>
2020-07-20 17:39:13 +0100

    Fixed the NASA POD page bug

--- lib/screens/nasapod.dart
+++ lib/screens/nasapod.dart
@@ -23,103 +23,113 @@ class _NasaPodState extends State<NasaPod> {
 
   @override
   void initState() {
-    Timer(Duration(seconds: 5), () => getImageData());
+    getImageData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        drawer: Drawer(
-          elevation: 20.0,
-          child: Padding(
-            padding: EdgeInsets.only(top: 40.0),
-            child: Column(
-              children: [
-                ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 25.0),
-                  leading: Icon(Icons.account_balance),
-                  title: Text('SpaceX Launch Timetable'),
-                  onTap: () {
-                    Navigator.pushNamed(context, spaceX_ID);
-                  },
-                ),
-                ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 25.0),
-                  leading: Icon(Icons.account_balance),
-                  title: Text('Mars stuff'),
-                  onTap: () {
-                    Navigator.pushNamed(context, mars_ID);
-                  },
-                ),
-              ],
-            ),
+    return Scaffold(
+      drawer: Drawer(
+        elevation: 20.0,
+        child: Padding(
+          padding: EdgeInsets.only(top: 40.0),
+          child: Column(
+            children: [
+              ListTile(
+                contentPadding: EdgeInsets.symmetric(horizontal: 25.0),
+                leading: Icon(Icons.account_balance),
+                title: Text('SpaceX Launch Timetable'),
+                onTap: () {
+                  Navigator.pushNamed(context, spaceX_ID);
+                },
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.symmetric(horizontal: 25.0),
+                leading: Icon(Icons.account_balance),
+                title: Text('Mars stuff'),
+                onTap: () {
+                  Navigator.pushNamed(context, mars_ID);
+                },
+              ),
+            ],
           ),
         ),
-        appBar: AppBar(
-          title: Text('Picture of the day'),
+      ),
+      appBar: AppBar(
+        title: Text('Picture of the day'),
+      ),
+      body: FutureBuilder(
+        future: Future.delayed(Duration(seconds: 1)),
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.done
+                ? PODContents(list: list, networkData: networkData)
+                : Center(child: CircularProgressIndicator()),
+      ),
+    );
+  }
+}
+
+class PODContents extends StatelessWidget {
+  const PODContents({
+    Key key,
+    @required this.list,
+    @required this.networkData,
+  });
+
+  final List list;
+  final NasaPODData networkData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: (MediaQuery.of(context).size.height) * 0.50,
+          width: (MediaQuery.of(context).size.width) * 0.99,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+            ),
+          ),
+          child: list[0] == null
+              ? Text('Media not Provided')
+              : list[4] == 'video'
+                  ? Text('Output is a video')
+                  : Image.network(
+                      list[0],
+                    ),
         ),
-        body: list[5] == 404
-            ? Center(
-                child: Text(
-                  'Data is not provided yet, check back later \n Error Code - ${list[4]}',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w700,
+        Text(
+          list[1] == null ? '' : list[1],
+          style: TextStyle(fontSize: 25),
+        ),
+        Text(
+          list[2] == null ? '' : list[2],
+          style: TextStyle(fontSize: 18),
+        ),
+        Expanded(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    list[3] == null ? '' : list[3],
+                    style: TextStyle(fontSize: 15),
                   ),
                 ),
-              )
-            : Column(
-                children: <Widget>[
-                  Container(
-                    height: (MediaQuery.of(context).size.height) * 0.50,
-                    width: (MediaQuery.of(context).size.width) * 0.99,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 2,
-                      ),
-                    ),
-                    child: list[0] == null
-                        ? Text('Media not Provided')
-                        : list[4] == 'video'
-                            ? Text('Output is a video')
-                            : Image.network(
-                                list[0],
-                              ),
-                  ),
-                  Text(
-                    list[1] == null ? '' : list[1],
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Text(
-                    list[2] == null ? '' : list[2],
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Expanded(
-                    child: ListView(
-                      children: <Widget>[
-                        Container(
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              list[3] == null ? '' : list[3],
-                              style: TextStyle(fontSize: 15),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  RaisedButton(
-                    child: Text('Tap Me'),
-                    onPressed: () {
-                      networkData.getData();
-                    },
-                  ),
-                ],
               ),
-      ),
+            ],
+          ),
+        ),
+        RaisedButton(
+          child: Text('Tap Me'),
+          onPressed: () {
+            networkData.getData();
+          },
+        ),
+      ],
     );
   }
 }
