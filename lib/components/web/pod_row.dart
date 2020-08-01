60894fbc6e011ec59587990081373de6d46ceeb0
Oran <42975732+ItsOran@users.noreply.github.com>
2020-08-01 08:54:10 +0100

    Changed the layout of the page for tne web version

---
+++ lib/components/web/pod_row.dart
@@ -0,0 +1,97 @@
+import 'package:SpacePortal/constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PODRow extends StatelessWidget {
+  const PODRow({
+    Key key,
+    @required this.orientation,
+    @required this.list,
+    @required this.box,
+  }) : super(key: key);
+
+  final Orientation orientation;
+  final List list;
+  final Box box;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 10),
+        Container(
+          height: orientation == Orientation.landscape ? 950.0 : null,
+          padding: EdgeInsets.all(10),
+          // color: Colors.green,
+          child: list[0] == 'video'
+              ? Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      launch(Hive.box('cacge').get('image'));
+                    },
+                    child: Text(
+                      '${Hive.box('cache').get('image')}',
+                      textAlign: TextAlign.center,
+                      style: kDetailsTS.copyWith(color: Colors.red),
+                    ),
+                  ),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(50.0),
+                  child: kIsWeb
+                      ? Image(
+                          image: NetworkImage(
+                            box.get('image'),
+                          ),
+                          fit: BoxFit.fill,
+                        )
+                      : CachedNetworkImage(
+                          imageUrl: box.get('image'),
+                          fit: BoxFit.fill,
+                        ),
+                ),
+        ),
+        Container(
+          width: (MediaQuery.of(context).size.width) * 0.50,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                child: Text(
+                  box.get('title'),
+                  style: kTitleDateTS.copyWith(fontSize: 40.0),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                child: Text(
+                  box.get('date'),
+                  style: kTitleDateTS.copyWith(fontSize: 25.0),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+                  child: Text(
+                    box.get('exp'),
+                    style: kDetailsTS.copyWith(
+                      letterSpacing: 1.0,
+                      fontSize: 18.0
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/nasapod.dart
+++ lib/screens/nasapod.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:SpacePortal/components/web/pod_row.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/foundation.dart';
@@ -130,6 +131,7 @@ class PODContents extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var orientation = (MediaQuery.of(context).orientation);
     return list == null
         ? Center(
             child: Text(
@@ -149,69 +151,81 @@ class PODContents extends StatelessWidget {
                   ),
                 ),
               )
-            : ListView(
+            : SingleChildScrollView(
                 physics: BouncingScrollPhysics(),
-                children: <Widget>[
-                  SizedBox(height: 10),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: list[0] == 'video'
-                        ? Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 8.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                launch(Hive.box('cacge').get('image'));
-                              },
-                              child: Text(
-                                '${Hive.box('cache').get('image')}',
-                                textAlign: TextAlign.center,
-                                style: kDetailsTS.copyWith(color: Colors.red),
-                              ),
-                            ),
-                          )
-                        : ClipRRect(
-                            borderRadius: BorderRadius.circular(50.0),
-                            //TODO: Fix this for the web version.
-                            child: kIsWeb
-                                ? Image.network(
-                                    box.get('image'),
-                                    fit: BoxFit.fill,
+                child: orientation == Orientation.landscape
+                    ? PODRow(orientation: orientation, list: list, box: box)
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 10),
+                          Container(
+                            height: orientation == Orientation.landscape
+                                ? 800.0
+                                : null,
+                            padding: EdgeInsets.all(10),
+                            // color: Colors.green,
+                            child: list[0] == 'video'
+                                ? Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 8.0),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        launch(Hive.box('cacge').get('image'));
+                                      },
+                                      child: Text(
+                                        '${Hive.box('cache').get('image')}',
+                                        textAlign: TextAlign.center,
+                                        style: kDetailsTS.copyWith(
+                                            color: Colors.red),
+                                      ),
+                                    ),
                                   )
-                                : CachedNetworkImage(
-                                    imageUrl: box.get('image'),
-                                    fit: BoxFit.fill,
+                                : ClipRRect(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                    child: kIsWeb
+                                        ? Image(
+                                            image: NetworkImage(
+                                              box.get('image'),
+                                            ),
+                                            fit: BoxFit.fill,
+                                          )
+                                        : CachedNetworkImage(
+                                            imageUrl: box.get('image'),
+                                            fit: BoxFit.fill,
+                                          ),
                                   ),
                           ),
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-                    child: Text(
-                      box.get('title'),
-                      style: kTitleDateTS.copyWith(fontSize: 25.0),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-                    child: Text(
-                      box.get('date'),
-                      style: kTitleDateTS.copyWith(fontSize: 18.0),
-                    ),
-                  ),
-                  Container(
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
-                      child: Text(
-                        box.get('exp'),
-                        style: kDetailsTS.copyWith(
-                          letterSpacing: 1.0,
-                        ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15.0, vertical: 8.0),
+                            child: Text(
+                              box.get('title'),
+                              style: kTitleDateTS.copyWith(fontSize: 25.0),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15.0, vertical: 8.0),
+                            child: Text(
+                              box.get('date'),
+                              style: kTitleDateTS.copyWith(fontSize: 18.0),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 15.0, vertical: 8.0),
+                              child: Text(
+                                box.get('exp'),
+                                style: kDetailsTS.copyWith(
+                                  letterSpacing: 1.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
-                ],
               );
   }
 }
