a122e880930f11b9fd9ab032d576bf4ef1aa033d
Oran <42975732+ItsOran@users.noreply.github.com>
2020-08-01 08:54:39 +0100

    Refactored Code

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,8 +4,6 @@ import 'package:SpacePortal/constants.dart';
 final ThemeData themeData = ThemeData(
   scaffoldBackgroundColor: kPrimaryBlack,
   primaryColorBrightness: Brightness.light,
-  // primaryColor: Colors.amber,
-  // accentColor: Colors.amber,
-  //accentColorBrightness: Brightness.light,
+  accentColorBrightness: Brightness.light,
   canvasColor: kPrimaryBlack,
 );
