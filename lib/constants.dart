25ece1f1ba83cc5b468e0c171629d758246e7f9a
Oran <42975732+ItsOran@users.noreply.github.com>
2020-08-04 09:58:56 +0100

    Removed Unused colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,12 +11,6 @@ const String kNoConnection_Page = 'noConnection_page';
 // New Colors
 const Color kPrimaryWhite = Colors.white;
 const Color kPrimaryBlack = Colors.black;
-// const Color kPrimaryDarkPurple = Color(0xFF1E1930);
-// const Color kAccentDogwoodRose = Color(0xFFCC296C);
-// const Color kAccentDarkCornflowerBlue = Color(0xFF25418A);
-// const Color kAccentPlumWeb = Color(0xFFE19EDD);
-// const Color kAccentWisteria = Color(0xFFAB95D7);
-//const Color kAccentAmber = Colors.amber;
 const Color kAccentSkyBlue = Color(0xFF00e9f2);
 
 // Theming for the app
@@ -27,10 +21,6 @@ const Color kAppBarColor = kPrimaryWhite;
 const Color kDropDownButtonColor = kPrimaryBlack;
 
 //Color Gradient
-BoxDecoration kAppbarBoxDecoration = BoxDecoration(
-  gradient: kGradient,
-);
-
 Gradient kGradient = LinearGradient(
   colors: kcolorGradient,
   begin: Alignment.centerRight,
@@ -54,4 +44,15 @@ TextStyle kTitleDateTS = GoogleFonts.notoSans(
 TextStyle kDetailsTS = GoogleFonts.notoSans(
   fontSize: 15.0,
   color: kPrimaryBlack,
+);
+
+TextStyle kMarsStatsStyle = GoogleFonts.notoSans(
+  fontSize: 15.0,
+  color: kPrimaryBlack,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle kTitleLargeTS = GoogleFonts.notoSans(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
 );
