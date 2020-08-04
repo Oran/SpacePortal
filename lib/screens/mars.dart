2292cbdc4b7ec3c04dd5f2add94bfb272e0718e9
Oran <42975732+ItsOran@users.noreply.github.com>
2020-08-04 09:59:31 +0100

    Added Stats

--- lib/screens/mars.dart
+++ lib/screens/mars.dart
@@ -226,28 +226,44 @@ class _MarsState extends State<Mars> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
               padding: EdgeInsets.only(top: 40.0, left: 10.0),
-              color: Colors.white,
-              child: Row(
+              child: Column(
                 children: [
-                  Padding(
-                    padding: EdgeInsets.only(right: 20.0),
-                    child: GestureDetector(
-                      child: Icon(
-                        Icons.subject,
-                        size: 35.0,
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 20.0),
+                        child: GestureDetector(
+                          child: Icon(
+                            Icons.subject,
+                            size: 30.0,
+                          ),
+                          onTap: () {
+                            _createDialog(context);
+                          },
+                        ),
                       ),
-                      onTap: () {
-                        _createDialog(context);
-                      },
-                    ),
+                      Text(
+                        'Mars Rover Images',
+                        style: kTitleLargeTS.copyWith(
+                          fontSize: 30.0,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    'Mars Rover Images',
-                    style: TextStyle(
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.w900,
-                    ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('Number of pictures: $numOfPics',
+                          style: kMarsStatsStyle),
+                      Text('Rover: $selectedRover', style: kMarsStatsStyle),
+                      Text('Camera: $selectedCam', style: kMarsStatsStyle),
+                      Text('Sol Days on mars: $selectedSol',
+                          style: kMarsStatsStyle),
+                    ],
                   ),
                 ],
               ),
