571fe22e1e6a1178f2d6fb4b19906ba12b9cda01
Oran <42975732+ItsOran@users.noreply.github.com>
2020-08-15 17:33:19 +0100

    Redesigned SpaceX Page

--- lib/components/spacex_page/spacex_content.dart
+++ lib/components/spacex_page/spacex_content.dart
@@ -1,69 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:SpacePortal/constants.dart';
 import 'package:SpacePortal/network/network.dart';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:provider/provider.dart';
 
 class SXContents extends StatelessWidget {
   SXContents({this.index});
   final int index;
-  
+
   @override
   Widget build(BuildContext context) {
     var spcx = Provider.of<List<dynamic>>(context);
-    return Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          spcx[index] == null
-              ? Text(
-                  'Data not Provided',
-                  style: kTitleDateTS,
-                )
-              : Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      spcx[index]['links']['mission_patch'] == null
-                          ? Text(
-                              '\'Mission Patch not provided\'',
-                              style: kTitleDateTS,
-                            )
-                          : Container(
-                              height: 200.0,
-                              width: 200.0,
-                              child: CachedNetworkImage(
-                                imageUrl: spcx[index]['links']['mission_patch'],
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            spcx[index] == null
+                ? Text(
+                    'Data not Provided',
+                    style: kTitleDateTS,
+                  )
+                : Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        spcx[index]['links']['mission_patch'] == null
+                            ? Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Container(
+                                  height: 80.0,
+                                  width: 80.0,
+                                  child: CachedNetworkImage(
+                                    imageUrl: kPlaceholderImage,
+                                  ),
+                                ),
+                              )
+                            : Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Container(
+                                  height: 80.0,
+                                  width: 80.0,
+                                  child: CachedNetworkImage(
+                                    imageUrl: spcx[index]['links']
+                                        ['mission_patch'],
+                                  ),
+                                ),
                               ),
-                            ),
-                      SizedBox(height: 15.0),
-                      Text(
-                        spcx[index]['mission_name'] == null
-                            ? '\'Mission Name not Provided\''
-                            : spcx[index]['mission_name'],
-                        textAlign: TextAlign.center,
-                        style: kTitleDateTS,
-                      ),
-                      Text(
-                          SpaceXData().parseString(
-                            spcx[index]['launch_date_utc'],
+                        SizedBox(height: 15.0),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text(
+                                spcx[index]['mission_name'] == null
+                                    ? '\'Mission Name not Provided\''
+                                    : spcx[index]['mission_name'],
+                                textAlign: TextAlign.center,
+                                style: kSpaceXTS,
+                              ),
+                              Text(
+                                  SpaceXData().parseString(
+                                    spcx[index]['launch_date_utc'],
+                                  ),
+                                  style: kSpaceXTS),
+                            ],
                           ),
-                          style: kTitleDateTS),
-                      spcx[index]['details'] == null
-                          ? Text(
-                              '\nDetails not Provided yet..',
-                              style: kTitleDateTS,
-                            )
-                          : Text(
-                              spcx[index]['details'],
-                              style: kDetailsTS,
-                            ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-        ],
+          ],
+        ),
       ),
     );
   }
