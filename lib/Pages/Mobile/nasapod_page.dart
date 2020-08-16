f51068ca521a56febcea8bd0ba19f4b1004e3f5e
Oran <42975732+ItsOran@users.noreply.github.com>
2020-08-16 21:51:00 +0100

    Removed Drawer

--- lib/Pages/Mobile/nasapod_page.dart
+++ lib/Pages/Mobile/nasapod_page.dart
@@ -1,53 +1,10 @@
 import 'package:SpacePortal/components/nasapod_page/pod_contents.dart';
 import 'package:flutter/material.dart';
-import 'package:SpacePortal/constants.dart';
 
 class NasaPod extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: kPrimaryDarkPurple,
-      drawer: Drawer(
-        elevation: 20.0,
-        child: Container(
-          color: kDrawerColor,
-          child: Padding(
-            padding: EdgeInsets.only(top: 40.0),
-            child: Column(
-              children: [
-                ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 25.0),
-                  leading: Icon(
-                    Icons.account_balance,
-                    color: kIconColor,
-                  ),
-                  title: Text(
-                    'SpaceX Launch Timetable',
-                    style: kDetailsTS,
-                  ),
-                  onTap: () {
-                    Navigator.pushNamed(context, kSpaceX_Page);
-                  },
-                ),
-                ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 25.0),
-                  leading: Icon(
-                    Icons.account_balance,
-                    color: kIconColor,
-                  ),
-                  title: Text(
-                    'Mars stuff',
-                    style: kDetailsTS,
-                  ),
-                  onTap: () {
-                    Navigator.pushNamed(context, kMars_Page);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
       body: PODContents(),
     );
   }
