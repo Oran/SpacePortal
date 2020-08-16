ceb37e9559bcfb9a6a077824665665c97d7a8c8f
Oran <42975732+ItsOran@users.noreply.github.com>
2020-08-16 16:33:55 +0100

    Refactored Mars Page

--- lib/Pages/Mobile/mars_page.dart
+++ lib/Pages/Mobile/mars_page.dart
@@ -1,9 +1,7 @@
+import 'package:SpacePortal/components/mars_page/mars_photos.dart';
 import 'package:flutter/material.dart';
 import 'package:SpacePortal/constants.dart';
-import 'package:SpacePortal/components/mars_page/image_viewer.dart';
-import 'package:flutter/foundation.dart';
 import 'package:SpacePortal/network/network.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 class Mars extends StatefulWidget {
   @override
@@ -87,19 +85,6 @@ class _MarsState extends State<Mars> {
           selectedRover = value;
         });
       },
-    );
-  }
-
-  Widget circle(BuildContext context, String text) {
-    return Center(
-      child: Container(
-        height: 30.0,
-        width: 30.0,
-        child: CircularProgressIndicator(
-          strokeWidth: 2,
-          valueColor: AlwaysStoppedAnimation<Color>(kPrimaryBlack),
-        ),
-      ),
     );
   }
 
@@ -260,87 +245,7 @@ class _MarsState extends State<Mars> {
                 ],
               ),
             ),
-            Expanded(
-              child: numOfPics == 0
-                  ? Center(
-                      child: Text(
-                        'No Images Provided',
-                        style: kTitleDateTS,
-                      ),
-                    )
-                  : GridView.builder(
-                      physics: BouncingScrollPhysics(),
-                      itemCount: list == null
-                          ? 0
-                          : list['photos'].length == 856
-                              ? 20
-                              : list['photos'].length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: (MediaQuery.of(context).orientation ==
-                                  Orientation.portrait)
-                              ? 2
-                              : 5),
-                      itemBuilder: (context, index) {
-                        return Container(
-                          decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey,
-                                spreadRadius: -20,
-                                blurRadius: 25,
-                                offset: Offset(0, 0),
-                              ),
-                              BoxShadow(
-                                color: Colors.grey,
-                                spreadRadius: -15,
-                                blurRadius: 15,
-                                offset: Offset(0, 0),
-                              ),
-                            ],
-                          ),
-                          child: list == null
-                              ? Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'Image is null',
-                                      style: kTitleDateTS.copyWith(
-                                          color: Colors.black),
-                                    ),
-                                  ),
-                                )
-                              : Padding(
-                                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(30.0),
-                                    child: GestureDetector(
-                                      child: Hero(
-                                        tag: 'tag' + index.toString(),
-                                        child: CachedNetworkImage(
-                                                imageUrl: list['photos'][index]
-                                                    ['img_src'],
-                                                fit: BoxFit.fill,
-                                                placeholder: circle,
-                                              ),
-                                      ),
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => ImageViewer(
-                                                index: index,
-                                                list: list['photos'][index]
-                                                    ['img_src']),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                ),
-                        );
-                      },
-                    ),
-            ),
+            MarsPhotos(numOfPics: numOfPics, list: list),
           ],
         ),
       ),
---
+++ lib/components/mars_page/mars_photos.dart
@@ -0,0 +1,110 @@
+import 'package:SpacePortal/components/mars_page/image_viewer.dart';
+import 'package:SpacePortal/constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class MarsPhotos extends StatelessWidget {
+  const MarsPhotos({
+    Key key,
+    @required this.numOfPics,
+    @required this.list,
+  }) : super(key: key);
+
+  final int numOfPics;
+  final list;
+
+  Widget circle(BuildContext context, String text) {
+    return Center(
+      child: Container(
+        height: 30.0,
+        width: 30.0,
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+          valueColor: AlwaysStoppedAnimation<Color>(kPrimaryBlack),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: numOfPics == 0
+          ? Center(
+              child: Text(
+                'No Images Provided',
+                style: kTitleDateTS,
+              ),
+            )
+          : GridView.builder(
+              primary: true,
+              cacheExtent: 200.0,
+              physics: BouncingScrollPhysics(),
+              itemCount: list == null
+                  ? 0
+                  : list['photos'].length == 856 ? 20 : list['photos'].length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: (MediaQuery.of(context).orientation ==
+                          Orientation.portrait)
+                      ? 2
+                      : 5),
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: -20,
+                        blurRadius: 25,
+                        offset: Offset(0, 0),
+                      ),
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: -15,
+                        blurRadius: 15,
+                        offset: Offset(0, 0),
+                      ),
+                    ],
+                  ),
+                  child: list == null
+                      ? Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Image is null',
+                              style: kTitleDateTS.copyWith(color: Colors.black),
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(30.0),
+                            child: GestureDetector(
+                              child: Hero(
+                                tag: 'tag' + index.toString(),
+                                child: CachedNetworkImage(
+                                  imageUrl: list['photos'][index]['img_src'],
+                                  fit: BoxFit.fill,
+                                  placeholder: circle,
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ImageViewer(
+                                        index: index,
+                                        list: list['photos'][index]['img_src']),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                );
+              },
+            ),
+    );
+  }
+}
