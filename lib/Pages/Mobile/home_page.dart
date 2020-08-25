4f7b25bf9433311d1871b50bb318475008e390a7
Oran <42975732+Oran@users.noreply.github.com>
2020-08-25 10:23:11 +0100

    Layout changes

--- lib/Pages/Mobile/home_page.dart
+++ lib/Pages/Mobile/home_page.dart
@@ -49,7 +49,7 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         width: double.infinity,
         child: Padding(
-          padding: EdgeInsets.only(top: 60.0),
+          padding: EdgeInsets.only(top: 30.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -108,7 +108,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     Container(
-                      height: (MediaQuery.of(context).size.height) * 0.45,
+                      height: (MediaQuery.of(context).size.height) * 0.49,
                       width: (MediaQuery.of(context).size.width),
                       child: MarsWeatherCard(),
                     ),
