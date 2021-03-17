71a341590a9356b1353832eaa84d6ee05bfbff98
Oran <42975732+Oran@users.noreply.github.com>
2021-03-17 03:23:59 +0000

    Added Perseverance rover to the app

--- lib/Network/MarsRoverNetwork.dart
+++ lib/Network/MarsRoverNetwork.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 final String _apiKey = 'pc7RPSAONSoBlJTGozeFT1EcaDa0mwXoD17XsKd3';
 
-final List<String> cam = [
+final List<String> oldCam = [
   'fhaz',
   'rhaz',
   'mast',
@@ -16,18 +16,37 @@ final List<String> cam = [
   'minites',
 ];
 
+final List<String> newCam = [
+  'EDL_RUCAM',
+  'EDL_RDCAM',
+  'EDL_DDCAM',
+  'EDL_PUCAM1',
+  'EDL_PUCAM2',
+  'NAVCAM_LEFT',
+  'NAVCAM_RIGHT',
+  'MCZ_RIGHT',
+  'MCZ_LEFT',
+  'FRONT_HAZCAM_LEFT_A',
+  'FRONT_HAZCAM_RIGHT_A',
+  'REAR_HAZCAM_LEFT',
+  'REAR_HAZCAM_RIGHT',
+  'SKYCAM',
+  'SHERLOC_WATSON',
+];
+
 final List<String> rover = [
   'curiosity',
   'opportunity',
   'spirit',
+  'perseverance'
 ];
 
 Uri marsRoverUrl = Uri.https(
   'api.nasa.gov',
-  '/mars-photos/api/v1/rovers/${rover[1]}/photos',
+  '/mars-photos/api/v1/rovers/${rover[0]}/photos',
   {
     'sol': '100',
-    'camera': '${cam[6]}',
+    'camera': '${oldCam[7]}',
     'api_key': '$_apiKey',
   },
 );
