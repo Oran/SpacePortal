29a9450ec845ca217e2dfdac9f0dd091573a67b3
Oran <42975732+Oran@users.noreply.github.com>
2021-04-06 02:15:28 +0100

    Added check for time diff to cache data

--- lib/Network/LaunchNetwork.dart
+++ lib/Network/LaunchNetwork.dart
@@ -1,21 +1,41 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:spaceportal/Models/LaunchData.dart';
-import 'data.dart';
+import 'package:localstorage/localstorage.dart';
 
 class LaunchNetwork {
-  Future getData() async {
-    // Uri url = Uri.https(
-    //   'll.thespacedevs.com',
-    //   '/2.2.0/launch/upcoming/',
-    //   {'format': 'json', 'limit': '20'},
-    // );
-    // http.Response response = await http.get(url);
-    // var decodedData = jsonDecode(response.body);
+  final storage = LocalStorage('data.json');
 
-    // For testing purposes use offline data
-    var decodedData = jsonData;
-    var data = Launches.fromList(decodedData['results']);
-    print(data.launchData[0].slug);
+  Future setDataToCache() async {
+    DateTime currentDateTime = DateTime.now();
+    DateTime cacheDate = DateTime.parse(storage.getItem('date'));
+    Duration difference = cacheDate.difference(currentDateTime);
+
+    print(cacheDate);
+    print(difference.inMinutes.toString() + ' mins');
+
+    // Checks if the time difference is -12 hours
+    if (difference.inHours <= -12) {
+      Uri url = Uri.https(
+        'll.thespacedevs.com',
+        '/2.2.0/launch/upcoming/',
+        {'format': 'json', 'limit': '20'},
+      );
+      // gets data from API
+      http.Response response = await http.get(url);
+      Map<String, dynamic> decodedData = jsonDecode(response.body);
+
+      // Caches http data to database
+      await storage.setItem('launchData', decodedData);
+      // Caches the current time
+      storage.setItem('date', currentDateTime.toString());
+    }
+  }
+
+  /// gets launch data from cache
+  Launches getDataFromCache() {
+    var cacheData = storage.getItem('launchData');
+    var data = Launches.fromList(cacheData['results']);
+    return data;
   }
 }
