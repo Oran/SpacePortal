becb6acf915e6983ee395a86be59e0d7232a8057
Oran <42975732+Oran@users.noreply.github.com>
2021-04-09 02:34:53 +0100

    Replaced countdown timer

--- lib/Pages/LaunchPage/Components/CountdownTimer.dart
+++ lib/Pages/LaunchPage/Components/CountdownTimer.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:spaceportal/Constants.dart';
 import 'package:spaceportal/Utils/CountDown.dart';
+import 'package:spaceportal/Utils/Functions.dart';
 
 class CountdownTimer extends StatefulWidget {
   final String time;
@@ -12,55 +12,19 @@ class CountdownTimer extends StatefulWidget {
 }
 
 class _CountdownTimerState extends State<CountdownTimer> {
-  String timer = '';
-  StreamSubscription? sub;
-
-  Duration timeDifference(String time) {
-    DateTime currentDateTime = DateTime.now();
-    DateTime parsedDate = DateTime.parse(time);
-    return currentDateTime.difference(parsedDate).abs();
-  }
-
-  StreamSubscription timeUpdate() {
-    var diff = timeDifference(widget.time);
-
-    CountDown cd = CountDown(diff);
-    return cd.stream.listen(null);
-  }
-
-
-  @override
-  void initState() {
-    super.initState();
-    sub = timeUpdate();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    sub?.onData((data) {
-      setState(() {
-        timer = data.toString().split('.')[0];
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    sub?.cancel();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: AutoSizeText(
-        '-' + timer + ' HMS',
-        textAlign: TextAlign.center,
-        style: kCardTS.copyWith(
-          fontSize: 15,
-        ),
-      ),
+    return Countdown(
+      duration: timeDifference(widget.time),
+      onFinish: () => print('finished'),
+      builder: (context, remaining) {
+        return AutoSizeText(
+          '-${remaining.toString().split('.')[0]} HMS',
+          style: kCardTS.copyWith(
+            fontSize: 15,
+          ),
+        );
+      },
     );
   }
 }
