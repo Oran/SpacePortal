e8dff3d856de9d91b102d2535d8670a13833acba
Oran <42975732+Oran@users.noreply.github.com>
2021-05-19 14:43:00 +0100

    Added dhms for the timer

--- lib/Pages/LaunchPage/Components/CountdownTimer.dart
+++ lib/Pages/LaunchPage/Components/CountdownTimer.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spaceportal/Constants.dart';
 import 'package:spaceportal/Utils/CountDown.dart';
@@ -12,6 +13,31 @@ class CountdownTimer extends StatefulWidget {
 }
 
 class _CountdownTimerState extends State<CountdownTimer> {
+  /// Converts a duration into DHMS format.
+  String _formatDuration(Duration d) {
+    var seconds = d.inSeconds;
+    final days = seconds ~/ Duration.secondsPerDay;
+    seconds -= days * Duration.secondsPerDay;
+    final hours = seconds ~/ Duration.secondsPerHour;
+    seconds -= hours * Duration.secondsPerHour;
+    final minutes = seconds ~/ Duration.secondsPerMinute;
+    seconds -= minutes * Duration.secondsPerMinute;
+
+    final List<String> tokens = [];
+    if (days != 0) {
+      tokens.add('${days}d');
+    }
+    if (tokens.isNotEmpty || hours != 0) {
+      tokens.add('${hours}h');
+    }
+    if (tokens.isNotEmpty || minutes != 0) {
+      tokens.add('${minutes}m');
+    }
+    tokens.add('${seconds}s');
+
+    return tokens.join(':');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Countdown(
@@ -19,9 +45,11 @@ class _CountdownTimerState extends State<CountdownTimer> {
       onFinish: () => print('finished'),
       builder: (context, remaining) {
         return AutoSizeText(
-          '-${remaining.toString().split('.')[0]} HMS',
+          'T -${_formatDuration(remaining)}',
+          textAlign: TextAlign.right,
           style: kCardTS.copyWith(
             fontSize: 15,
+            shadows: null,
           ),
         );
       },
