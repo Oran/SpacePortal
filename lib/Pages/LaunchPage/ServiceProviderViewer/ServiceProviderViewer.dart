1c949f85cc5ef906a00e787dcc6ee76a8ec677a3
Oran <42975732+Oran@users.noreply.github.com>
2021-06-21 07:49:37 +0100

    Base info for ServiceProviderViewer

--- lib/Pages/LaunchPage/ServiceProviderViewer/ServiceProviderViewer.dart
+++ lib/Pages/LaunchPage/ServiceProviderViewer/ServiceProviderViewer.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:spaceportal/Constants.dart';
 import 'package:spaceportal/Network/LaunchNetwork.dart';
 
 class ServiceProviderViewer extends StatefulWidget {
@@ -18,24 +20,40 @@ class _ServiceProviderViewerState extends State<ServiceProviderViewer> {
       builder: (context, AsyncSnapshot snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return Scaffold(
-            backgroundColor: Colors.amber[100],
             appBar: AppBar(
               elevation: 0,
               backgroundColor: Colors.white,
+              centerTitle: true,
+              title: Text(
+                'Launch Provider',
+                style: kDetailsTS.copyWith(
+                  fontSize: 20,
+                ),
+              ),
             ),
             body: Container(
               height: (MediaQuery.of(context).size.height),
               width: (MediaQuery.of(context).size.width),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+              child: ListView(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                physics: BouncingScrollPhysics(),
+                
                 children: [
                   Container(
                     height: 200,
                     width: 200,
-                    child: Image.network(snapshot.data['logo_url']),
+                    child: CachedNetworkImage(
+                      imageUrl: snapshot.data['logo_url'],
+                    ),
                   ),
-                  Text(snapshot.data['name'].toString()),
+                  Text(snapshot.data['name']),
+                  Text(snapshot.data['type']),
+                  Text(snapshot.data['founding_year']),
+                  Text(snapshot.data['country_code']),
+                  Text(snapshot.data['abbrev']),
+                  Text(snapshot.data['administrator']),
+                  Text(snapshot.data['description']),
                 ],
               ),
             ),
