31bcd7a27e101790c5ea6755b87c1d8d2f734ace
Oran <42975732+Oran@users.noreply.github.com>
2021-07-15 10:24:07 +0100

    Replaced FutureBuilder with Consumer in loading_page

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -3,6 +3,7 @@ import 'package:spaceportal/network/apod_network.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spaceportal/network/articles_network.dart';
 import 'package:spaceportal/network/launch_network.dart';
+import 'package:spaceportal/pages/loading_page/loading_page.dart';
 import 'package:spaceportal/utils/functions.dart';
 import 'package:spaceportal/utils/generate_blurhash.dart';
 
@@ -25,3 +26,6 @@ final blurhashProvider = FutureProvider.family(
 final whiteBalanceProvider = FutureProvider.family(
   (ref, url) => checkImgColor(url),
 );
+
+/// Loads all the futures in loading page
+final loadingPageProvider = FutureProvider((ref) => LoadingPage().allFutures());
