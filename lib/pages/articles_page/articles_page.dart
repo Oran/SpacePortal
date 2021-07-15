6813d3ba6397a1f3c1ba5311df48d7e72933c986
Oran <42975732+Oran@users.noreply.github.com>
2021-07-15 11:11:48 +0100

    Replaced FlutureBuilder in articles_page

--- lib/pages/articles_page/articles_page.dart
+++ lib/pages/articles_page/articles_page.dart
@@ -12,105 +12,98 @@ class ArticlesPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     var articles = watch(articleProvider);
-    return FutureBuilder(
-      future: checkImgColor(articles.data[0].imageUrl),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: kPrimaryWhite,
-              elevation: 0,
-              centerTitle: true,
-              title: Text(
-                'Articles',
-                style: TextStyle(
-                  color: changeColorAppBar(snapshot.data),
+    var whiteBalance = watch(whiteBalanceProvider(articles.data[0].imageUrl));
+    return whiteBalance.when(
+        data: (wb) => Scaffold(
+              appBar: AppBar(
+                backgroundColor: kPrimaryWhite,
+                elevation: 0,
+                centerTitle: true,
+                title: Text(
+                  'Articles',
+                  style: TextStyle(
+                    color: changeColorAppBar(wb),
+                  ),
+                ),
+                iconTheme: IconThemeData(
+                  color: changeColorAppBar(wb),
+                ),
+                flexibleSpace: Consumer(
+                  builder: (context, watch, child) {
+                    var provider =
+                        watch(blurhashProvider(articles.data[0].imageUrl));
+                    return provider.when(
+                      data: (data) => FadeInAppBar(value: data),
+                      loading: () => Container(),
+                      error: (e, s) {
+                        print(e);
+                        print(s);
+                        return Container(
+                          color: Colors.grey[100],
+                        );
+                      },
+                    );
+                  },
                 ),
               ),
-              iconTheme: IconThemeData(
-                color: changeColorAppBar(snapshot.data),
-              ),
-              flexibleSpace: Consumer(
-                builder: (context, watch, child) {
-                  var provider =
-                      watch(blurhashProvider(articles.data[0].imageUrl));
-                  return provider.when(
-                    data: (data) => FadeInAppBar(value: data),
-                    loading: () => Container(),
-                    error: (e, s) {
-                      print(e);
-                      print(s);
-                      return Container(
-                        color: Colors.grey[100],
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-            body: Container(
-              child: ListView.builder(
-                padding: EdgeInsets.only(top: 20),
-                physics: BouncingScrollPhysics(),
-                itemCount: articles.data.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    child: ArticleCard(
-                      image: CachedNetworkImage(
-                        imageUrl: articles.data[index].imageUrl,
-                        fit: BoxFit.cover,
-                        memCacheHeight: 700,
-                        memCacheWidth: 1000,
-                        progressIndicatorBuilder: (context, url, progress) {
-                          return Center(
-                            child: Container(
-                              height: 50,
-                              width: 50,
-                              child: CircularProgressIndicator(
-                                backgroundColor: Colors.white,
-                                valueColor: AlwaysStoppedAnimation(
-                                  Colors.black,
+              body: Container(
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 20),
+                  physics: BouncingScrollPhysics(),
+                  itemCount: articles.data.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: ArticleCard(
+                        image: CachedNetworkImage(
+                          imageUrl: articles.data[index].imageUrl,
+                          fit: BoxFit.cover,
+                          memCacheHeight: 700,
+                          memCacheWidth: 1000,
+                          progressIndicatorBuilder: (context, url, progress) {
+                            return Center(
+                              child: Container(
+                                height: 50,
+                                width: 50,
+                                child: CircularProgressIndicator(
+                                  backgroundColor: Colors.white,
+                                  valueColor: AlwaysStoppedAnimation(
+                                    Colors.black,
+                                  ),
                                 ),
                               ),
-                            ),
-                          );
-                        },
-                        errorWidget: (context, url, error) {
-                          return Container();
+                            );
+                          },
+                          errorWidget: (context, url, error) {
+                            return Container();
+                          },
+                        ),
+                        text: articles.data[index].title,
+                        textStyle: kCardTS.copyWith(
+                          fontSize: 20,
+                        ),
+                        publishedDate:
+                            articles.data[index].publishedAt.split('T')[0],
+                        source: articles.data[index].newsSite,
+                        height: 250.0,
+                        onPressed: () {
+                          try {
+                            launch(
+                              articles.data[index].url,
+                              forceWebView: true,
+                              enableJavaScript: true,
+                              enableDomStorage: true,
+                            );
+                          } catch (e) {
+                            print(e);
+                          }
                         },
                       ),
-                      text: articles.data[index].title,
-                      textStyle: kCardTS.copyWith(
-                        fontSize: 20,
-                      ),
-                      publishedDate:
-                          articles.data[index].publishedAt.split('T')[0],
-                      source: articles.data[index].newsSite,
-                      height: 250.0,
-                      onPressed: () {
-                        try {
-                          launch(
-                            articles.data[index].url,
-                            forceWebView: true,
-                            enableJavaScript: true,
-                            enableDomStorage: true,
-                          );
-                        } catch (e) {
-                          print(e);
-                        }
-                      },
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
-          );
-        } else {
-          return Scaffold(
-            body: flareLoadingAnimation(),
-          );
-        }
-      },
-    );
+        loading: () => Scaffold(body: flareLoadingAnimation()),
+        error: (e, s) => Text('Error'));
   }
 }
