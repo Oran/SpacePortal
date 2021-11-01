9c2bfe3f77c95503c99bb1b5f570f5d0ff0a58c6
Oran <42975732+Oran@users.noreply.github.com>
2021-11-01 17:56:50 +0000

    Updated: Colour now matches the icon

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primaryLight = Color(0xffFFFFFF);
-  static const primaryDark = Color(0xff151515);
+  static const primaryDark = Color(0xff000C17);
 
   static const accentLight = Color(0xff000000);
   static const accentDark = Color(0xffE5E5E5);
 
   static const error = Color(0xffff424a);
-  static const cardDark = Color(0xff2A2A2A);
+  static const cardDark = Color(0xff001930);
   static const cardLight = Color(0xffE5E5E5);
 }
